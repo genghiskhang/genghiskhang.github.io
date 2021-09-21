6044fc25843cc0cbcf4ae51864a75f34f91637bd
genghiskhang <khang11475@gmail.com>
2021-09-21 08:05:47 -0400

    updated navbar to flexbox

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-window.addEventListener("load", event => {
-    for (var i = 0; i < document.images.length; i++) {
-        if (!document.images[i].complete && document.images[i].naturalHeight == 0) {
-            document.images[i].src = 'assets/default-avatar.jpg';
-        }
-    }
-}); 
+// window.addEventListener("load", event => {
+//     for (var i = 0; i < document.images.length; i++) {
+//         if (!document.images[i].complete && document.images[i].naturalHeight == 0) {
+//             document.images[i].src = 'assets/default-avatar.jpg';
+//         }
+//     }
+// }); 
