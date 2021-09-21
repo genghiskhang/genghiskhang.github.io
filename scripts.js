41c129a6573b56d0baebd2eccaee2aa2fb88fd99
genghiskhang <khang11475@gmail.com>
2021-09-21 08:26:41 -0400

    test script

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-window.onload = function() {
+window.addEventListener("load", event => {
     for (var i = 0; i < document.images.length; i++) {
+        console.log(document.images[i].complete);
         if (!document.images[i].complete) {
             document.images[i].src = 'assets/default-avatar.jpg';
         }
     }
-}
+}); 
