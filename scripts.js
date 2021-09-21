83c25eb99befb16a7942ccf2748cc96d4d82e095
genghiskhang <khang11475@gmail.com>
2021-09-21 08:57:44 -0400

    image error fix

--- scripts.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", event => {
-    for (var i = 0; i < document.images.length; i++) {
-        console.log(document.images[i].complete);
-        if (!document.images[i].complete) {
-            document.images[i].src = 'assets/default-avatar.jpg';
-        }
-    }
-}); 
