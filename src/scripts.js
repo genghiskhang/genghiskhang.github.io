be97c3b2007474b659e357cdaee7a459c63b71d3
genghiskhang <khang11475@gmail.com>
2021-09-20 20:39:28 -0400

    initial push

---
+++ src/scripts.js
@@ -0,0 +1,12 @@
+// window.onload = function () {
+//     document.getElementById('header-navbar').innerHTML += 
+//     '<p><b>Urbana High School Robotics</b></p>' +
+//     '<ul>' +
+//         '<li><a id="active-page" href="index.html"><b>Home</b></a>' +
+//         '<li><a href="about.html"><b>About Us</b></a>' +
+//         '<li><a href="team.html"><b>The Team</b></a>' +
+//         '<li><a href="signup.html"><b>Sign Up</b></a>' +
+//         '<li><a href="media.html"><b>Media</b></a>' +
+//         '<li><a href="sponsors.html"><b>Sponsors</b></a>' +
+//     '</ul>';
+// }
