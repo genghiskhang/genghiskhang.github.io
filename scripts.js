162a22481117f207e8dae09847602348d98420db
genghiskhang <khang11475@gmail.com>
2021-09-20 21:46:41 -0400

    fix unloaded image script

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", event => {
     var images = document.images;
-    for (var i = 0; i < images; i++) {
+    for (var i = 0; i < images.length; i++) {
         if (!images[i].complete && images[i].naturalHeight == 0) {
             images[i].src = 'assets/default-avatar.jpg';
         }
