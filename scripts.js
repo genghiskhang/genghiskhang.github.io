823488740189e4863e2313c3e5c9c3e1ed4aef3b
genghiskhang <khang11475@gmail.com>
2021-09-21 08:18:22 -0400

    testing scripts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-window.addEventListener("load", event => {
-    for (var i = 0; i < document.images.length; i++) {
-        if (!document.images[i].complete && document.images[i].naturalHeight == 0) {
-            document.images[i].src = 'assets/default-avatar.jpg';
-        }
+// window.addEventListener("load", event => {
+//     for (var i = 0; i < document.images.length; i++) {
+//         if (!document.images[i].complete && document.images[i].naturalHeight == 0) {
+//             document.images[i].src = 'assets/default-avatar.jpg';
+//         }
+//     }
+// }); 
+
+var image = new Image();
+image.onload = function() {
+    if (!image.complete) {
+        image.src = 'assets/default-avatar.jpg';
     }
-}); 
+}
