bfbb3d9192cfda7cf1e9cb7cc3b675e7bb2c6bdf
genghiskhang <khang11475@gmail.com>
2021-09-21 08:23:45 -0400

    test script

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", event => {
+window.onload = function() {
     for (var i = 0; i < document.images.length; i++) {
         if (!document.images[i].complete) {
             document.images[i].src = 'assets/default-avatar.jpg';
