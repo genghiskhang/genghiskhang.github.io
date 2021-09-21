5e1f6aa732b9202b71b1c7e285ea90825f02fecf
genghiskhang <khang11475@gmail.com>
2021-09-20 21:19:31 -0400

    test image script

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
-// window.onload = function () {
-//     document.getElementById('header-navbar').innerHTML += 
-//     '<p><b>Urbana High School Robotics</b></p>' +
-//     '<ul>' +
-//         '<li><a id="active-page" href="index.html"><b>Home</b></a>' +
-//         '<li><a href="about.html"><b>About Us</b></a>' +
-//         '<li><a href="team.html"><b>The Team</b></a>' +
-//         '<li><a href="signup.html"><b>Sign Up</b></a>' +
-//         '<li><a href="media.html"><b>Media</b></a>' +
-//         '<li><a href="sponsors.html"><b>Sponsors</b></a>' +
-//     '</ul>';
-// }
+window.addEventListener("load", event => {
+    var image = document.querySelector('img');
+    var isLoaded = image.complete && image.naturalHeight !== 0;
+    if (!isLoaded) {
+        image.src = 'assets/default-avatar.jpg';
+    }
+});
