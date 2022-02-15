ed86d45985ae34175c4b3cb2ee49974f4c408174
genghiskhang <khang11475@gmail.com>
2022-02-15 12:38:20 -0500

    Initial commit

---
+++ js/header.js
@@ -0,0 +1,38 @@
+const headerShrinkWidth = 1290;
+header = document.getElementsByTagName('header')[0];
+
+function shrinkHeader() {
+    if (document.documentElement.scrollTop > 50 || document.body.clientWidth <= headerShrinkWidth) {
+        header.className = 'shrink';
+    } else {
+        header.className = '';
+    }
+}
+
+// Shrink header on mobile and resized window
+body.onload = function () {
+    if (body.clientWidth <= headerShrinkWidth) {
+        header.className = 'shrink';
+    } else {
+        header.className = '';
+    }
+};
+body.onresize = function () {
+    if (body.clientWidth <= headerShrinkWidth) {
+        header.className = 'shrink';
+    } else {
+        header.className = '';
+    }
+
+    if (body.clientWidth > headerShrinkWidth) {
+        if (nav.className == 'open') {
+            body.style.overflow = null;
+        }
+    } else {
+        if (nav.className == 'open') {
+            body.style.overflow = 'hidden';
+        }
+    }
+};
+
+document.addEventListener('scroll', shrinkHeader);
