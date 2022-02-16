fa45050af0d1926ffee009a82f946b3f9bb1e67c
genghiskhang <khang11475@gmail.com>
2022-02-15 21:32:09 -0500

    Updated header and footer on all pages

--- js/header.js
+++ js/header.js
@@ -2,15 +2,15 @@ window.onscroll = scrollFunction;
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById('logo').style.maxHeight = '75px';
-    document.getElementById('logo').style.margin = '10px 0px 10px 0px';
+    document.getElementById('team-logo').style.maxHeight = '75px';
+    document.getElementById('team-logo').style.margin = '10px 0px 10px 0px';
     document.getElementById('team-name').style.fontSize = '2vw';
     document.getElementById('team-name').style.marginTop = '0px';
     document.getElementById('team-number').style.fontSize = '0vw';
   }
   else {
-    document.getElementById('logo').style.maxHeight = '150px';
-    document.getElementById('logo').style.margin = '20px 0px 20px 0px';
+    document.getElementById('team-logo').style.maxHeight = '150px';
+    document.getElementById('team-logo').style.margin = '20px 0px 20px 0px';
     document.getElementById('team-name').style.fontSize = '3vw';
     document.getElementById('team-name').style.marginTop = '20px';
     document.getElementById('team-number').style.fontSize = '2vw';
