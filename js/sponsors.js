af2cd6a0a7f318a4581d94de54117bb5f37a6db7
genghiskhang <khang11475@gmail.com>
2022-02-16 12:49:00 -0500

    Set up sponsors page and added some member pictures

---
+++ js/sponsors.js
@@ -0,0 +1,15 @@
+window.onload = loadSponsors;
+
+function loadSponsors() {
+    fetch('../assets/sponsors/spnsrs.txt').then(function(response) {
+        return response.text();
+    }).then(function(data) {
+        let sponsorList = data.split('\r\n');
+        for (var i = 0; i < sponsorList.length; i++) {
+            if (i % 2 == 0) {
+                $('.spnsr-main > table > tbody').append(`<tr></tr>`);
+            }
+            $('.spnsr-main > table > tbody > tr').append(`<td><img class="spnsr-img" src="../assets/sponsors/${sponsorList[i].split('-')[1].trim()}.png"></td>`);
+        }
+    })
+}
--- js/team.js
+++ js/team.js
@@ -1,16 +1,23 @@
 window.onload = createAvatars;
 
 function createAvatars() {
-    fetch('../assets/global/members.txt').then(function(response) {
+    fetch('../assets/member-avatars/members.txt').then(function(response) {
         return response.text();
     }).then(function(data) {
         let memberList = data.split('\r\n');
         for (var i = 0; i < memberList.length; i++) {
             $('.members').append('<div class="member-profile">' +
-                                    `<img class="avatar" src="../assets/member-avatars/${1}">` +
+                                    `<img class="avatar" src="../assets/member-avatars/${memberList[i].split('-')[0].split(' ')[0].toLowerCase() + memberList[i].split('-')[0].split(' ')[1][0].toLowerCase()}.jpg">` +
                                     `<h1 class="member-info">${memberList[i].split('-')[0].trim()}</h1>` +
                                     `<h2 class="member-position">${memberList[i].split('-')[1].trim()}</h2>` +
                                  '</div>');
         }
     });
+    // let images = document.images;
+    // for (var i = 0; i < images.length; i++) {
+    //     if (images[i].naturalWidth == 0) {
+    //         console.log('fail')
+    //         images[i].src = '../assets/member-avatars/default-avatar.jpg';
+    //     }
+    // }
 }
