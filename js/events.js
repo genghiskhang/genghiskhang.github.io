a5ac66cab0c4a71fabd5d185c722564171f1ab7d
genghiskhang <khang11475@gmail.com>
2022-02-17 19:38:45 -0500

    Update website

---
+++ js/events.js
@@ -0,0 +1,30 @@
+window.onload = loadEvents;
+
+function loadEvents() {
+    // fetch('https://www.thebluealliance.com/apidocs/v3/status').then(res => console.log(res));
+    // fetch('https://www.thebluealliance.com/apidocs/v3/status', {
+    //     headers:{
+    //         'X-TBA-Auth-Key':'YHiq0QSn5AGgnjmEIka4VFLzQ8JpDWj8VKEHJ8F3BICF1zwMZoeaIYbsfHIaApjK',
+    //         Accept:'application/json',
+    //                'Content-Type': 'application/json',
+    //                'Access-Control-Allow-Origin': '*'
+    //     },
+    //     mode:'no-cors'
+    // }).then(function(response) {
+    //     console.log(response);
+    // }).catch(function(error) {
+    //     console.log(erro);
+    // })
+
+    // $.ajax({
+    //     dataType:'json',
+    //     url:'https://www.thebluealliance.com/apidocs/v3',
+    //     headers:{
+    //         'X-TBA-Auth-Key':'YHiq0QSn5AGgnjmEIka4VFLzQ8JpDWj8VKEHJ8F3BICF1zwMZoeaIYbsfHIaApjK'
+    //     },
+    //     dataType:'jsonp'
+    // }).done(function(data) {
+    //     console.log(data);
+    // }).fail(function(error) {
+    // });
+}
