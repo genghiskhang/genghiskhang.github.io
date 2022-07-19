48462a2c24310ffb972297e988c9a058829b3e69
Khang Le <khang11475@gmail.com>
2022-07-19 12:36:50 -0400

    portfolio start

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    let images = document.images;
-    for (var i = 0; i < images.length; i++) {
-        if (images[i].naturalWidth == 0) {
-            console.log('fail')
-            images[i].src = '../assets/member-avatars/default-avatar.jpg';
-        }
-    }
-}
