344f03f2413bba92ea6c2315a5e8fdf83d734f11
Khang Le <khang11475@gmail.com>
2022-11-19 18:42:49 -0500

    moved navbar back to top

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,25 @@
-document.onmousemove = function (event) {
-    var mouseX = event.clientX;
-    var winWidth = document.documentElement.clientWidth;
+// window.onscroll = function(event) {
+//     // console.log(document.body.scrollTop);
+//     console.log(document.documentElement.scrollTop);
+// };
+
+$(document).on("scroll", function(event) {
+    // console.log(document.documentElement.scrollTop);
+    // console.log($(".navbar-link").length);
+    // console.log($(".navbar-link").offset().top);
+    // console.log($(document));
+    var top = document.documentElement.scrollTop;
+    console.log($("#about").offset().top);
+    // if (top >= $("#about").offset().top())
+});
+// document.onmousemove = function (event) {
+//     var mouseX = event.clientX;
+//     var winWidth = document.documentElement.clientWidth;
     
-    if (mouseX <= Math.ceil(winWidth/10)) {
-        $("nav").addClass("open");
-    }
-    else {
-        $("nav").removeClass("open");
-    }
-}
+//     if (mouseX <= Math.ceil(winWidth/10)) {
+//         $("nav").addClass("open");
+//     }
+//     else {
+//         $("nav").removeClass("open");
+//     }
+// }
