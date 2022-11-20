5da1cc8f3151e7a15708caa8b22a80e3b199707e
Khang Le <khang11475@gmail.com>
2022-11-20 03:15:56 -0500

    navbar link highlighting fix

--- js/navbar.js
+++ js/navbar.js
@@ -4,34 +4,34 @@ $(document).on("scroll", function(event) {
     // console.log($(".navbar-link").get(0).id);
 
     // title
-    if (top < $("#about").offset().top) {
+    if (top < $("#about").offset().top - 50) {
         $(".navbar-link").get(0).id = "";
     }
     // about
-    else if (top >= $("#about").offset().top && top < $("#experience").offset().top)  {
+    else if (top >= $("#about").offset().top - 50 && top < $("#experience").offset().top - 50)  {
         $(".navbar-link").get(0).id = "active";
         $(".navbar-link").get(1).id = "";
     }
     // experience
-    else if (top >= $("#experience").offset().top && top < $("#projects").offset().top) {
+    else if (top >= $("#experience").offset().top - 50 && top < $("#projects").offset().top - 50) {
         $(".navbar-link").get(0).id = "";
         $(".navbar-link").get(1).id = "active";
         $(".navbar-link").get(2).id = "";
     }
     // projects
-    else if (top >= $("#projects").offset().top && top < $("#skills").offset().top) {
+    else if (top >= $("#projects").offset().top - 50 && top < $("#skills").offset().top - 50) {
         $(".navbar-link").get(1).id = "";
         $(".navbar-link").get(2).id = "active";
         $(".navbar-link").get(3).id = "";
     }
     // skills
-    else if (top >= $("#skills").offset().top && top < $("#contact").offset().top) {
+    else if (top >= $("#skills").offset().top - 50 && top < $("#contact").offset().top - 50) {
         $(".navbar-link").get(2).id = "";
         $(".navbar-link").get(3).id = "active";
         $(".navbar-link").get(4).id = "";
     }
     // contact
-    else if (top >= $("#contact").offset().top) {
+    else if (top >= $("#contact").offset().top - 50) {
         $(".navbar-link").get(3).id = "";
         $(".navbar-link").get(4).id = "active";
     }
