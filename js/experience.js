ccedeeac23833b77b40109e8cf3df685f2070c3b
Khang Le <khang11475@gmail.com>
2022-11-24 00:10:56 -0500

    experience blocks slide open and close

--- js/experience.js
+++ js/experience.js
@@ -1,8 +1,3 @@
 $(".experience-item").on("click", function() {
-    if ($(this).find(".experience-body").hasClass("collapse")) {
-        $(this).find(".experience-body").removeClass("collapse");
-    }
-    else {
-        $(this).find(".experience-body").addClass("collapse");
-    }
+    $($(this).children()[1]).slideToggle();
 });
