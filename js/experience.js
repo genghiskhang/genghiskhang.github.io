deeae3f039506b3540551237c80827ba31c403c9
Khang Le <khang11475@gmail.com>
2022-11-25 23:52:16 -0500

    experience section formatted

--- js/experience.js
+++ js/experience.js
@@ -1,3 +1,9 @@
 $(".experience-item").on("click", function() {
     $($(this).children()[1]).slideToggle();
+    if ($($($(this).children()[0]).children()[1]).hasClass("flip")) {
+        $($($(this).children()[0]).children()[1]).removeClass("flip");
+    }
+    else {
+        $($($(this).children()[0]).children()[1]).addClass("flip");
+    }
 });
