594cb6fbd95ce596170379103e619eda61938d97
Khang Le <khang11475@gmail.com>
2022-11-29 17:12:57 -0500

    experience.js code cleanup

--- js/experience.js
+++ js/experience.js
@@ -1,9 +1,7 @@
 $(".experience-item").on("click", function() {
-    $($(this).children()[1]).slideToggle();
-    if ($($($(this).children()[0]).children()[1]).hasClass("flip")) {
-        $($($(this).children()[0]).children()[1]).removeClass("flip");
-    }
-    else {
-        $($($(this).children()[0]).children()[1]).addClass("flip");
-    }
+    var experienceItem = $($(this).children()[1]);
+    var experienceItemArrow = $($($(this).children()[0]).children()[1]);
+    
+    experienceItem.slideToggle();
+    experienceItemArrow.toggleClass("flip");
 });
