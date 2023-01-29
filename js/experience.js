120dad706240c39d8963047c656284bcec4259df
Khang Le <khang11475@gmail.com>
2023-01-29 01:11:06 -0500

    info update and page formatting

--- js/experience.js
+++
@@ -1,7 +0,0 @@
-$(".experience-item").on("click", function() {
-    var experienceItem = $($(this).children()[1]);
-    var experienceItemArrow = $($($(this).children()[0]).children()[1]);
-    
-    experienceItem.slideToggle();
-    experienceItemArrow.toggleClass("flip");
-});
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+// Spacing
+$(document).ready(function() {
+    var lastSection = $($("main").children()[$("main").children().length - 1]).css({"paddingBottom":"6rem"});
+    console.log(lastSection);
+});
+
+// Navbar
 function noNeg(number) {
     if (number < 0) {
         return 0;
@@ -31,6 +38,27 @@ $(document).on("scroll", function(event) {
         }
         else {
             $(".navbar-link").get(0).id = "active";
+        }
+    }
+});
+
+// Experience
+$(".experience-item").on("click", function() {
+    var experienceItem = $($(this).children()[1]);
+    var experienceItemArrow = $($($(this).children()[0]).children()[1]);
+    
+    experienceItem.slideToggle();
+    experienceItemArrow.toggleClass("flip");
+});
+
+// Skills
+$(document).on("scroll", function() {
+    var bottom = $(document).scrollTop() + $(window).height();
+    var numSkills = $(".skills-item").length;
+
+    for (var i = 0; i < numSkills; i++) {
+        if (bottom >= $($(".skills-item").get(i)).offset().top + 50) {
+            $($(".skills-item").get(i)).addClass("skills-item-fade");
         }
     }
 });
